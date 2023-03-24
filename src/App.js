fe91f77dff7ff81c8d304180511b1e5286f87786
Sai-Rane <sai.rane729@gmail.com>
2023-03-24 18:08:59 +0530

    firstCommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+// import "./App.css";
+import React, { useEffect, useState } from "react";
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-alpine-dark.css";
 
 function App() {
+  const [rowData, setRowData] = useState([
+    { Make: "Ford", Model: "Ford", Price: 2000 },
+    { Make: "Toyota", Model: "Toyota", Price: 3000 },
+    { Make: "ABC", Model: "ABC", Price: 4000 },
+  ]);
+  // const rowData = [
+  //   { Make: "Ford", Model: "Ford", Price: 2000 },
+  //   { Make: "Toyota", Model: "Toyota", Price: 3000 },
+  //   { Make: "ABC", Model: "ABC", Price: 4000 },
+  // ];
+
+  const [columnDefs, setColumnDefs] = useState([
+    { field: "Make", sortable: true, filter: true },
+    { field: "Model", sortable: true, filter: true },
+    { field: "Price", sortable: true, filter: true },
+  ]);
+
+  // const columnDefs = [
+  //   { field: "Make" },
+  //   { field: "Model" },
+  //   { field: "Price" },
+  // ];
+
+  // useEffect(() => {
+  //   fetch("https://www.ag-grid.com/example-assets/row-data.json")
+  //     .then((result) => result.json())
+  //     .then((rowData) => setRowData(rowData));
+  // }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ag-theme-alpine-dark" style={{ height: 500 }}>
+      <AgGridReact
+        rowData={rowData}
+        columnDefs={columnDefs}
+        rowSelection="multiple"
+        animateRows={true}
+      />
     </div>
   );
 }
